df53a874e57d73e2b42f0cb024bdfeb6d674e310
RLungWu <larry14789@gmail.com>
2026-02-26 02:02:10 +0800

    Add the push_task to the class.

--- 01-thread-pool/main.cpp
+++ 01-thread-pool/main.cpp
@@ -6,8 +6,9 @@
 #include <queue>
 #include <thread>
 #include <vector>
+#include <chrono>
 
-const int MAX_THREADS = 10;
+const int MAX_THREADS = std::thread::hardware_concurrency() / 2;
 
 class ThreadPool {
 private:
@@ -16,7 +17,18 @@ private:
   std::mutex mtx;
   std::condition_variable cv;
   bool stop = false;
-
+  
+  void worker_thread(){
+    while(true){
+      std::unique_lock<std::mutex> lock(mtx);  
+      cv.wait(lock, [this]{return !tasks.empty() || stop;});
+      if (stop && tasks.empty()) return;
+      auto task = tasks.front();
+      tasks.pop();
+      lock.unlock();
+      task();
+    }
+  };
 public:
   ThreadPool(std::size_t threads_num) {
     if (threads_num > MAX_THREADS) {
@@ -26,18 +38,59 @@ public:
 
     this->workers.reserve(threads_num);
     for (size_t i = 0; i < threads_num; i++) {
-      this->workers.push_back(std::thread());
+      this->workers.push_back(std::thread(&ThreadPool::worker_thread, this));
     }
   };
+  
+  ~ThreadPool(){
+    this->stop = true;
+    this->cv.notify_all();
+    for (auto& worker : workers){
+      worker.join();
+    }
+  }
 
   void push_task(std::function<void()> task){
-    this->tasks.push(task);  
-    this->cv.notify_one();
+    {
+      std::lock_guard<std::mutex> lock(mtx);
+      tasks.push(task);
+    }
+    cv.notify_one();
   };
 };  
 
 int main() {
-  ThreadPool pool(4);
-  std::cout << "ThreadPool created with 4 threads." << std::endl;
+  ThreadPool* pool = new ThreadPool(4);
+  std::cout << "ThreadPool created with " << MAX_THREADS << " threads." << std::endl;
+
+  auto start = std::chrono::high_resolution_clock::now();
+  
+  for (int i = 0; i < 10; i++) {
+    pool->push_task([](){
+      // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      volatile long long sum = 0;
+      for (int i = 0; i < 1000000; i++) sum += i;
+    });
+  }
+
+  delete pool;
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = end - start;
+  std::cout << "Run time: " << diff.count() << " seconds" << std::endl;
+
+
+  auto start2 = std::chrono::high_resolution_clock::now();
+  
+  for (int i = 0; i < 10; i++) {
+    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    volatile long long sum = 0;
+    for (int i = 0; i < 1000000; i++) sum += i;
+  }
+
+  auto end2 = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff2 = end2 - start2;
+  std::cout << "Run time: " << diff2.count() << " seconds" << std::endl;
+
   return 0;
 }
